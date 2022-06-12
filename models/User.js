5dcdc40acfb72b53cf10176c8d72827f3112dd3a
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-06-11 21:14:30 -0400

    added properties to User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,46 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtuals('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
